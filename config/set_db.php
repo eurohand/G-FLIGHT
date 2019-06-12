595debe097e054381481b61134746df6232155ef
eurohand <eurohand@naver.com>
2019-06-12 21:39:25 +0900

    db update

--- config/set_db.php
+++ config/set_db.php
@@ -1,16 +1,18 @@
 <?php
 //데이터 베이스 설정
 
-// $mydb['host'] = 'thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-// $mydb['user'] = 'wnvaak8rsg3pchfw';
-// $mydb['pass'] = 'pimtbqjggrbf1tiv';
-// $mydb['name'] = 'clp3n4gm746vekgy';
 
 $url = getenv('mysql://wnvaak8rsg3pchfw:pimtbqjggrbf1tiv@thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/clp3n4gm746vekgy');
 $dbparts = parse_url($url);
 
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
+
+$hostname = 'thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username = 'wnvaak8rsg3pchfw';
+$password = 'pimtbqjggrbf1tiv';
+$database = 'clp3n4gm746vekgy';
+
+// $hostname = $dbparts['host'];
+// $username = $dbparts['user'];
+// $password = $dbparts['pass'];
+// $database = ltrim($dbparts['path'],'/');
 ?>
