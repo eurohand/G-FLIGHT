328fbf81a1d50d6e5ab27b397d2226b81cbc7abe
eurohand <eurohand@naver.com>
2019-06-13 02:16:31 +0900

    123

--- ranking.php
+++ ranking.php
@@ -161,7 +161,7 @@ mysqli_select_db($connect, $database);
   let Dot = function(x, y, r, color){
     let div = document.createElement('div');
     div.className = 'dot';
-    div.style.background-color = "#" + color + color + color;
+    div.style.backgroundColor = "#" + color + color + color;
     div.style.width = r + 'px';
     div.style.height = r + 'px';
     div.style.position = 'absolute';
