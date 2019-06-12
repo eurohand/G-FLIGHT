12b448e422b0dd11039bede627ec8ac2c6f9f91b
eurohand <eurohand@naver.com>
2019-06-12 21:23:38 +0900

    re

--- config/set_db.php
+++ config/set_db.php
@@ -1,10 +1,8 @@
 <?php
 //데이터 베이스 설정
-$url = getenv('mysql://wnvaak8rsg3pchfw:pimtbqjggrbf1tiv@thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/clp3n4gm746vekgy');
-$dbparts = parse_url($url);
 
-$mydb['host'] = $dbparts['host'];
-$mydb['user'] = $dbparts['user'];
-$mydb['pass'] = $dbparts['pass'];
-$mydb['name'] = ltrim($dbparts['path'],'/');
+$mydb['host'] = 'thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$mydb['user'] = 'wnvaak8rsg3pchfw';
+$mydb['pass'] = 'pimtbqjggrbf1tiv';
+$mydb['name'] = 'clp3n4gm746vekgy';
 ?>
