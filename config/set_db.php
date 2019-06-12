904e0e7bc8ac03f8e5d9679df9036e150286c0b5
eurohand <eurohand@naver.com>
2019-06-12 21:20:10 +0900

    db update

--- config/set_db.php
+++ config/set_db.php
@@ -1,8 +1,10 @@
 <?php
 //데이터 베이스 설정
-$mydb['host'] = "localhost";
-$mydb['name'] = "gflight";
-$mydb['user'] = "root";
-$mydb['pass'] = "qwe!@#";
-	
+$url = getenv('mysql://wnvaak8rsg3pchfw:pimtbqjggrbf1tiv@thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/clp3n4gm746vekgy');
+$dbparts = parse_url($url);
+
+$mydb['host'] = $dbparts['host'];
+$mydb['user'] = $dbparts['user'];
+$mydb['pass'] = $dbparts['pass'];
+$mydb['name'] = ltrim($dbparts['path'],'/');
 ?>
