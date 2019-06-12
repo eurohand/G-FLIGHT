9f9af1278e6f60bf675f980b0fde001ee6156343
eurohand <eurohand@naver.com>
2019-06-12 22:09:57 +0900

    front

--- ranking.php
+++ ranking.php
@@ -16,50 +16,73 @@ mysqli_select_db($connect, $database);
 <html lang="ko" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Ranking</title>
+    <title>G-FLGHT (RANKING)</title>
   </head>
   <body>
+  <style>
+    #container{
+      width: 1500px;
+      height: 700px;
+      border: "10px solid gray";
+      background-color: "black";
+      color: "white";
+    }
 
-  <table>
-  <caption>
-  <colgroup>
-    <col style="width:10%">
-    <col style="width:20%">
-    <col style="width:50%">
-    <col style="width:50%">
-    </colgroup>
-  <thead>
-<tr>
-    <th scope="col">순위</th>
-    <th scope="col">이름</th>
-    <th scope="col">점수</th>
-    <th scope="col">날자</th>
-</tr>
-</thead>
-<tbody>
-<?php
-  $sql="SELECT * FROM ranking ORDER BY score DESC";
-  $re = mysqli_query($connect, $sql);
-  $i = 1;
-  while($data=mysqli_fetch_array($re)){
-?>
-<tr>
-    <th><?php echo $i;?></th>
-    <th><?php echo $data['name']; ?></th>
-    <th><?php echo $data['score']; ?></th>
-    <th><?php echo $data['date']; ?></th>
-</tr>
+  </style>
 
-<?php
-    $i++;
-  }
-  mysqli_close($connect);
-  ?>
 
-</tbody>
+  <div id="container">
+    <table>
+      <caption>
+        <colgroup>
+          <col style="width:10%">
+          <col style="width:20%">
+          <col style="width:50%">
+          <col style="width:50%">
+        </colgroup>
+        <thead>
+          <tr>
+            <th scope="col">순위</th>
+            <th scope="col">이름</th>
+            <th scope="col">점수</th>
+            <th scope="col">날자</th>
+            </tr>
+        </thead>
 
-  </caption>
-  </table>
+        <tbody>
+        <?php
+          $sql="SELECT * FROM ranking ORDER BY score DESC";
+          $re = mysqli_query($connect, $sql);
+          $i = 1;
+          while($data=mysqli_fetch_array($re)){
+        ?>
+        <tr>
+            <th><?php echo $i;?></th>
+            <th><?php echo $data['name']; ?></th>
+            <th><?php echo $data['score']; ?></th>
+            <th><?php echo $data['date']; ?></th>
+        </tr>
+
+        <?php
+            $i++;
+          }
+          mysqli_close($connect);
+          ?>
+
+        </tbody>
+      </caption>
+    </table>
+  </div>
+
+
+
+
+
+
+
+
+
+ 
 
   <div class="goBack">돌아가기</div>
   </body>
